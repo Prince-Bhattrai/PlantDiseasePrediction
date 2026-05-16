4cc71f34027aef81878596eb1e979e275bed3ba3
Prince <bhattraiprince39@gmail.com>
2026-05-17 02:31:35 +0545

    added timeout to model api calling

--- backend/controllers/historyController.js
+++ backend/controllers/historyController.js
@@ -29,6 +29,7 @@ export const predict = async (req, res) => {
             formData,
             {
                 headers: formData.getHeaders(),
+                timeout:120000
             }
         );
 
